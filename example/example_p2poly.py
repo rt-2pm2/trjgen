a000a5ea3da6f53147082f8c1d534b57e0fe1123
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-06-10 00:12:25 -0700

    Created 'example' folder; Moved example inside

--- example/example_p2poly.py
+++ example/example_p2poly.py
@@ -5,7 +5,11 @@ Created on Sat Jun  8 02:54:42 2019
 
 @author: rt-2pm2
 """
+import sys
+sys.path.insert(0, '../trjgen')
+
 import numpy as np
+
 from trjgen import trjgen_helpers as tjh
 from trjgen import trjgen as tj
 from trjgen import pwpoly as pw
@@ -24,9 +28,9 @@ print('Number of constraint on the flat output = {:d}.'.format(nconstr))
 
 # Build the constraint matrix
 X = np.array([
-        [ 0,     1.0,    2.0],
-        [ 0,     0.01,   0.0],
-        [ 0,   -10.0,    0.0],
+        [ 0,     1.0,    0.6],
+        [ 0,     0.02,   0.0],
+        [ 0,   -9.0,     0.0],
         [ 0,   np.nan,   0.0],
         ])
 
@@ -38,29 +42,39 @@ Y = np.array([
         ])
 
 Z = np.array([
-        [ 0,   0.0,     -0.2],
+        [ 0,   0.0,     -0.3],
         [ 0,   0.0,      0.0],
         [ 0,   0.0,      0.0],
         [ 0,   np.nan,   0.0],
         ])
 
+W = np.array([
+        [ 0,   np.nan,      0.0],
+        [ 0,   np.nan,      0.0],
+        [ 0,   np.nan,      0.0],
+        [ 0,   np.nan,      0.0],
+        ])
+
 # Times (Absolute and intervals)
-knots = np.array([i for i in range(X.shape[1])])
+knots = np.array([0, 1.5, 3]) # One second each piece
 Dt = knots[1:len(knots)] - knots[0:len(knots)-1]
 
 # Compute the polynomials pieces
 (solx, nullx, resx, polysx) = tj.interpolPolys(X, ndeg, knots, True)
 (soly, nully, resy, polysy) = tj.interpolPolys(Y, ndeg, knots, True)
 (solz, nullz, resz, polysz) = tj.interpolPolys(Z, ndeg, knots, True)
+(solw, nullw, resw, polysw) = tj.interpolPolys(W, ndeg, knots, True)
 
 # Instantiate the Piecewise polynomials
 ppx = pw.PwPoly(polysx, knots)
 ppy = pw.PwPoly(polysy, knots)
 ppz = pw.PwPoly(polysz, knots)
+ppw = pw.PwPoly(polysw, knots)
 
 # Check (Evaluate polynomial)
-tv = np.linspace(0,2,100);
-(Xtj, Ytj, Ztj, Wtj) = tjh.TrajFromPW(tv, [0,1,2], pwpolx=ppx, pwpoly=ppy, \
-                                        pwpolz=ppz, pwpolw = None)
+tv = np.linspace(0,max(knots),100);
+(Xtj, Ytj, Ztj, Wtj, Zbtj) = tjh.TrajFromPW(tv, [0,1,2], pwpolx=ppx, \
+                                        pwpoly=ppy, pwpolz=ppz, pwpolw = ppw)
 
 
+tj.pp2file(Dt, polysx, polysy, polysz, polysw, "./poly.txt")
